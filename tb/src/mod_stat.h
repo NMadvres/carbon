ee7275ca96c0cf215f81ec0bc351a5800c1cd9cd
newtonniuzz <newton@newtonniu.NEBULA-MATRIX.COM>
2021-10-20 14:53:49 +0800

    add files

--- tb/src/mod_stat.h
+++ tb/src/mod_stat.h
@@ -1,60 +1,23 @@
-///////////////////////////////////////////////////////////
-//Project: Carbon
-//Description: SystemC 虚拟项目1
-//Module: mod_stat.h
-//Description:  收包模块
-//Group: Nebula-Matrix 预研组
-//Author: Jeff/Shawn
-//Hierarchy：内部无嵌套模块的功能模块
-////////////////////////////////////////////////////////////
-
-//1.防重定义
 #ifndef __MOD_STAT_H__
 #define __MOD_STAT_H__
-
-//2.include工具库
 #include "comm_def.h"
-  
-//using namespace std;   
+////////////////////////////////////////////////////////
+// Project： SystemC虚拟项目
+// Module:   mod_stat
+// Description: mod_stat stat统计模块h文件
+// Group：预研组
+// Author: Newton
+// Date: 2021.10.14 第一版
+// Hierarchy : 编号，索引公共库
+////////////////////////////////////////////////////////
 class mod_stat: public sc_module
 {
-
-//3.端口声明
-  public: 
-    sc_in<int> in_clk_cnt;
-    //sc_in<tb_cfg> tb_cfg_in; 
-    //sc_in<pkt_desc> pkt_stat_in;  //sample 1
-    std::array< sc_in<pkt_desc>, G_INTEL_NUM > in_pkt_stat;  
-
-//4.信号声明
-    //std::array< pkt_desc, NO_PORTS > pkt_stat_buf; 
-
-//5.模块声明
-    mod_stat(sc_module_name name); 
-    SC_HAS_PROCESS(mod_stat);
-
-//6.函数声明    
-    void pkt_dump();
-
+    public:    
+        mod_stat(sc_module_name name);
+        SC_HAS_PROCESS(mod_stat);
+    public:
+        array<sc_in<s_pkt_desc> *, g_inter_num>        in_pkt_ports;
+        sc_in<int>                                     in_clk_cnt;  
 };
 
-mod_stat::
-mod_stat(sc_module_name name):sc_module(name){
-    SC_METHOD(pkt_dump);
-    for(int i=0; i < G_INTEL_NUM; i++){
-        sensitive<< in_pkt_stat[i];
-    }
-}
-
-void mod_stat::
-pkt_dump() {
-    for(int i=0; i < G_INTEL_NUM; i++){
-        if ((in_pkt_stat[i].read().fsn >= 0) && 
-           in_pkt_stat[i].event()){
-            cout << "@"<< in_clk_cnt <<"_clks stat rcv  <=:"
-                 << in_pkt_stat[i];
-        }
-    }
-}
-
-#endif //__MOD_STAT_H__
+#endif
--- tb/src/top_tb.h
+++ tb/src/top_tb.h
@@ -1,84 +1,56 @@
-///////////////////////////////////////////////////////////
-//Project: Carbon
-//Description: SystemC 虚拟项目1
-//Module: tb_top.h
-//Description: 测试台 top 文件，实例化各测试模块
-//Group: Nebula-Matrix 预研组
-//Author: Shawn
-//Hierarchy：
-//             tb_top __:stim
-//                    |_:stat
-//                    |_:test_ctrl
-////////////////////////////////////////////////////////////
-
-//1.防重定义
 #ifndef __TOP_TB_H__
 #define __TOP_TB_H__
+#include "comm_def.h"
+#include "mod_stim.h"
+#include "mod_stat.h"
+#include "mod_testctrl.h"
 
-//2.include工具库
-#include "comm_def.h"      //通用定义
-
-//3.include子模块库
-#include "mod_stim.h"      //激励模块
-#include "mod_stat.h"      //统计模块
-#include "mod_test_ctrl.h" //测试控制模块
-
-//using namespace std;   
+////////////////////////////////////////////////////////
+// Project： SystemC虚拟项目
+// Module:   top_tb
+// Description: top_tb 顶层互联
+// Group：预研组
+// Author: Newton
+// Date: 2021.10.14 第一版
+// Hierarchy : 编号，索引公共库
+////////////////////////////////////////////////////////
 class top_tb: public sc_module
 {
+  public: //例化及互联部分
+    mod_stim  *stim_mod;
+    mod_stat  *stat_mod;
+    mod_testcrtl *testcrtl_mod;
+    array<sc_in<s_pkt_desc> *, g_inter_num>   in_pkt_ports;  //用于透传连线ing的输入端口信号,在顶层连接发包器
+    array<sc_out<s_pkt_desc> *,g_inter_num>   out_pkt_ports; //用于透传连线egr的输出端口信号,在顶层连接stat
+    sc_in_clk    in_glb_clk;                                 //用于透传连线testctrl输入信号,在顶层连接testctrl
+    sc_out<int>  out_clk_cnt;                                //用于透传连线testctrl输出信号,在顶层连接testctrl
+    sc_in<int>   in_clk_cnt;                                 //全局时钟计数，用于互联  
+  public:
+    top_tb(sc_module_name name):sc_module(name)
+    {
+      //例化
+      stim_mod =new mod_stim("stim_mod");
+      stat_mod =new mod_stat("stat_mod");
+      testcrtl_mod =new mod_testcrtl("mod_testcrtl");
+      for(int i =0; i < g_inter_num; i++)
+      {
+        in_pkt_ports[i]  =  new sc_in<s_pkt_desc>(); 
+        out_pkt_ports[i] = new sc_out<s_pkt_desc>();            
+      }
+      //互联
+      //ing的入口和egr的出口，连线透传到顶层，待更高层进行连接
+      for(int i =0; i < g_inter_num; i++)
+      {
+        stim_mod->out_pkt_ports[i]->bind(*out_pkt_ports[i]);
+        stat_mod->in_pkt_ports[i]->bind(*in_pkt_ports[i]);
+      }
+      testcrtl_mod->in_glb_clk(in_glb_clk); //透传clk连线
+      testcrtl_mod->out_clk_cnt(out_clk_cnt);
+      //cycle计数信号透传
+      stim_mod->in_clk_cnt(in_clk_cnt);
+      stat_mod->in_clk_cnt(in_clk_cnt);
+    }
 
-//4.端口声明
-  public: 
-    sc_in_clk in_clk;                   //全局时钟输入
-//    sc_out<int> clk_cnt_out;          //全局时钟计数输出，由test_ctrl根据全局时钟输入产生
-//    sc_out<dut_cfg> dut_cfg_out;      //由test_ctrl读取文件产生
-//    sc_in<pkt_desc> pkt_stat_in;      //sample 1个
-//    sc_out<pkt_desc> pkt_stim_out;    //sample 1个
-    std::array<  sc_in<pkt_desc>, G_INTEL_NUM > in_pkt_stat;   //由stat模块接收的报文
-    std::array< sc_out<pkt_desc>, G_INTEL_NUM > out_pkt_stim;  //由stim产生的激励报文
-
-//5.信号声明
-    sc_signal<int> sig_gclk_cnt;           //由test_ctrl产生，需要输出给外部和tb_top内部其他子模块
-//    sc_signal<tb_cfg> tb_cfg_sig;      //由test_ctrl读取文件产生，输送给其他的testbench内部模块
-
-//5.模块声明
-    //tb_top(sc_module_name name); 
-    //SC_HAS_PROCESS(tb_top);
-//6.函数声明
-//    void tb_top_process()
-
-//7.子模块声明
-
-    mod_stim *stim_inst;
-    mod_stat *stat_inst;
-    mod_test_ctrl *test_ctrl_inst;
-
-    SC_CTOR(top_tb){
-    //8.子模块例化    
-        stim_inst = new mod_stim("stim_inst");
-        stat_inst = new mod_stat("stat_inst");
-        test_ctrl_inst = new mod_test_ctrl("test_ctrl_inst");
-    //stim_mod stim_inst("stim_init");
-    //stat_mod stat_inst("stat_inst");
-    //test_ctrl_mod test_ctrl_inst("test_ctrl_inst");
-    
-    //9.信号连接 
-        stim_inst->in_clk_cnt(sig_gclk_cnt);
-        //stim_inst->tb_cfg_in(tb_cfg_sig);
-        for (int i=0;i<G_INTEL_NUM;i++){
-            stim_inst->out_pkt_stim[i](out_pkt_stim[i]);
-        }
-        stat_inst->in_clk_cnt(sig_gclk_cnt);
-        //stat_inst->tb_cfg_in(tb_cfg_sig);
-        for (int i=0;i<G_INTEL_NUM;i++){
-            stat_inst->in_pkt_stat[i](in_pkt_stat[i]);
-        }
-        test_ctrl_inst->in_clk(in_clk);
-        test_ctrl_inst->out_clk_cnt(sig_gclk_cnt);
-        //test_ctrl_inst->dut_cfg_out(dut_cfg_out);
-        //test_ctrl_inst->tb_cfg_out(tb_cfg_sig);
-        //clk_cnt_out = gclk_cnt;
-   };
+  SC_HAS_PROCESS(top_tb);
 };
-
-#endif  //__TOP_TB_H__
+#endif
