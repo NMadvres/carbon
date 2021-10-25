04197ac1eb77cdae619e2c2f3a3810666bdc33ab
John790118 <goodcross@outlook.com>
2021-10-25 15:22:00 +0800

    john-v0.3 delete redundant code

--- tb/src/mod_stim.cpp
+++ tb/src/mod_stim.cpp
@@ -1,9 +1,4 @@
-
-
-#include <stdlib.h>
-#include <time.h>
 #include "mod_stim.h"
-#include "comm_def.h"
 
 void port_fifo::pkt_in(const s_pkt_desc& data_pkt)
 {
@@ -59,6 +54,14 @@ void mod_stim :: stim_prc()
     array<token_bucket,G_INTER_NUM> port_token_bucket;
     array<token_bucket,FLOW_RULE_TAB_SIZE> flow_token_bucket;
     array<int,FLOW_RULE_TAB_SIZE> flow_sn;
+    s_pkt_desc pkt_desc_tmp;  
+
+    ofstream pkt_sender_file;
+//    pkt_sender_file.open("pkt_sender_file.log");
+    pkt_sender_file.open(pkt_sender_filename);
+
+    pkt_send_count = 0;
+    token_count = 0;
 
     for(int i=0; i<G_INTER_NUM; i++)
     {
@@ -81,19 +84,9 @@ void mod_stim :: stim_prc()
     {
         flow_sn[i] = 0;
     }
-
  
-    ofstream pkt_sender_file;
-//    pkt_sender_file.open("pkt_sender_file.log");
-    pkt_sender_file.open(pkt_sender_filename);
-
-    pkt_send_count = 0;
-    token_count = 0;
-
-    srand((unsigned)time(NULL));
     wait(8);
     while(1)
-//    while(pkt_send_count < SEND_FILE_CYCLE)
     {
         if(token_count < G_FREQ)
         {
@@ -173,84 +166,7 @@ void mod_stim :: stim_prc()
             }
         }
 
-
         wait();
-
-        // wait for 1 to 3 clock periods
-        //wait(1+(rand()%3));
-
     }
-
     pkt_sender_file.close();
 }
-
-#if 0
-#include "packet_gen.h"
-//激励包长应从顶层获取，先配置固定256B，稍后再改
-packet_gen_module::packet_gen_module(string name, global_config_c *glb_cfg):sc_module(name)
-{
-
-    m_cycle_cnt =0;
-    m_packet_id =0;
-    m_cfg = glb_cfg;
-    
-    output.resize(g_m_inter_num);
-    for(int i=0; i < g_m_inter_num; i++)
-    {
-        output[i] =new sc_fifo_out<TRANS>;
-    }
-    //init shape
-    packet_shape.resize(g_m_inter_num);
-    for(int i=0; i < g_m_inter_num; i++)
-    {
-        packet_shape[i] = new comm_shape_func(1000, 9620, 13, 10);
-    }
-
-    //stat
-    string debug_file = name + string("_debug.log");
-    m_bw_stat =new comm_stat_bw(m_cfg, debug_file,g_m_inter_num);
-    
-    SC_METHOD(packet_gen_process);
-    sensitive << clk.pos();
-
-    
-
-}
-void packet_gen_module::packet_gen_process()
-{
-   m_cycle_cnt++;   
-   //stat
-   if((m_cycle_cnt !=0) && (m_cycle_cnt % (m_cfg->stat_period *100) ==0))
-   {
-       m_bw_stat->print_bw_info(m_cycle_cnt);
-   }
-   
-   //填桶
-    if((m_cycle_cnt !=0) &&(m_cycle_cnt % 10 ==0))
-    {
-        for(int i=0; i < g_m_inter_num; i++)
-        {
-            packet_shape[i]->add_token(13);  //10cc填充13个
-        }    
-    }
-    
-   //4端口轮询
-   for(int i=0; i < g_m_inter_num; i++)
-   {
-        if(packet_shape[i]->shape_status(256)) //包长是否够
-        {
-            m_packet_id++;  
-            TRANS new_trans(new trans_type);
-            new_trans->packet_id = m_packet_id;
-            new_trans->port_id =i;
-            new_trans->packet_len = 256;
-            new_trans->valid_len =  256;
-            new_trans->is_sop = true;
-            new_trans->is_eop = true;
-            output[i]->nb_write(new_trans);
-            //stat
-            m_bw_stat->record_bw_info(i, new_trans->valid_len, true);
-        }
-   }   
-}
-#endif//if 0
