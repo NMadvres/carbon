9e7f3cb319c5984ffe3edf075914f75a956787e2
Shawn918 <wxy13@hotmail.com>
2021-11-15 13:20:36 +0800

    add const int G_FLOW_NUM = 16; into comm_def.h

--- src/comm_def.h
+++ src/comm_def.h
@@ -27,6 +27,7 @@ using namespace std;
 const int G_FREQ_MHZ = 100; //100M HZ
 const int G_INTER_NUM = 4;
 const int G_QUE_NUM = 16;
+const int G_FLOW_NUM = 16;
 const int G_CELL_LEN = 64;
 const int G_PRI_NUM = 4;
 const int G_PE_MPPS = 50;
